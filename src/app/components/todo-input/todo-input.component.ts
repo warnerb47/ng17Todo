d77f8684acc71dff799e9ad0e3e7011d705fbf4e
warnerb47 <taphagaye2001.mg@gmail.com>
2024-01-19 10:57:12 +0000

    feat: add header

---
+++ src/app/components/todo-input/todo-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-input.component.html',
+  styleUrl: './todo-input.component.scss'
+})
+export class TodoInputComponent {
+
+}
--- src/app/modules/today-tasks/today-tasks.component.ts
+++ src/app/modules/today-tasks/today-tasks.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TodoInputComponent } from '../../components/todo-input/todo-input.component';
+import { TodoComponent } from '../../components/todo/todo.component';
 
 @Component({
   selector: 'app-today-tasks',
   standalone: true,
-  imports: [],
+  imports: [ TodoInputComponent, TodoComponent ],
   templateUrl: './today-tasks.component.html',
   styleUrl: './today-tasks.component.scss'
 })
