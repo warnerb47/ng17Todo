7549d0242b52430fd2d38effd12c6c083246b550
warnerb47 <taphagaye2001.mg@gmail.com>
2024-01-19 14:35:48 +0000

    feat: add navItems icons

--- src/app/components/side-bar/components/nav-items/nav-items.component.ts
+++ src/app/components/side-bar/components/nav-items/nav-items.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'side-bar-nav-items',
   standalone: true,
-  imports: [ RouterModule ],
+  imports: [ RouterModule, MatIconModule ],
   templateUrl: './nav-items.component.html',
   styleUrl: './nav-items.component.scss'
 })
