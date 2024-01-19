e5003bd48aa33ebcaf6aba8a887cd436830af3aa
warnerb47 <taphagaye2001.mg@gmail.com>
2024-01-19 10:14:50 +0000

    feat(sidebar): add navItems

---
+++ src/app/components/side-bar/components/nav-items/nav-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'side-bar-nav-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-items.component.html',
+  styleUrl: './nav-items.component.scss'
+})
+export class NavItemsComponent {
+
+}
--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserComponent } from './components/user/user.component';
+import { NavItemsComponent } from './components/nav-items/nav-items.component';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [ UserComponent ],
+  imports: [ UserComponent, NavItemsComponent ],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
