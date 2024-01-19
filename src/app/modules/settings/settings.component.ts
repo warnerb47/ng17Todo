98b56ed4b38ebd433c21b447610ab747fd446383
warnerb47 <taphagaye2001.mg@gmail.com>
2024-01-19 15:07:17 +0000

    feat: add settings icon

--- src/app/modules/settings/settings.component.ts
+++ src/app/modules/settings/settings.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
