31626c5ba70d6c4fd0479ba245d988711baa58b9
warnerb47 <taphagaye2001.mg@gmail.com>
2024-01-19 14:55:55 +0000

    feat: add todo icon

--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-todo-input',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './todo-input.component.html',
   styleUrl: './todo-input.component.scss'
 })
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
